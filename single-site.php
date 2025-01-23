69758e1255d2fb9ec3d0a3bcba82267070af5dc8
Neihos <detourbe.nathanael@gmail.com>
2025-01-23 18:59:28 +0100

    mise en forme de la page présentations de chaque site

--- single-site.php
+++ single-site.php
@@ -26,17 +26,64 @@ if ( have_posts() ) :
     <article class="preview-site">
 
         <section class="siteOnPage">
-            <div class="single-content">            
-                <h1 class="post-title"><?php the_title(); ?></h1>
-                <img 
-                    src="<?php echo esc_url( $image_url ); ?>"
-                >
-                <div class="publication-year">Date : <?php echo get_the_date('j F Y'); ?></div>
-                <div class="description"><?php echo get_field('description'); ?></div>
-                <a class="linkToSite" href="<?php echo get_field('lien_vers_le_site'); ?>">Voir le site</a>           
-            </div>        
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <div class="single-content"> 
+               
+                <div class="smartphoneContainer">             
+                    <img class="smartphoneImg" src="/wp-content/themes/novaneos/assets/images/smartphone.webp" alt="smartphone">
+                    <div class="inSmartphone">
+                        <img src="<?php echo esc_url( $img_smartphone_url ); ?>" alt="aperçu du site en mode smartphone">
+                    </div>
+                </div> 
+                <div class="computerContainer"> 
+                    <img class="computerImg" src="/wp-content/themes/novaneos/assets/images/laptop.webp" alt="Ordinateur">
+                    <div class="inComputer">
+                        <img src="<?php echo esc_url( $image_url ); ?>" alt="aperçu du site en mode desktop">
+                    </div>
+                </div>  
+                <div class="tabletteContainer">         
+                    <img class="tabletteImg" src="/wp-content/themes/novaneos/assets/images/tablette.webp" alt="tablette">
+                    <div class="inTablette">
+                        <img src="<?php echo esc_url( $img_tablette_url ); ?>" alt="aperçu du site en mode tablette">
+                    </div>
+                </div>
+
+            </div>
+                 
         </section>
-                
+
+        <section class="infoSite">
+            <span>Description :</span>
+            <div class="description"><?php echo get_field('description'); ?></div>
+            <div class="publication-year">Date : <?php echo get_the_date('j F Y'); ?></div>
+            <a class="linkToSite" href="<?php echo get_field('lien_vers_le_site'); ?>"><i class="fa-solid fa-globe"></i>  Voir le site <i class="fa-solid fa-globe"></i></a> 
+        </section>
+        
+        <div class="sliderContainer">
+            <div class="arrows">
+                <a class="arrow-left" href="<?php echo $previousPostUrl; ?>">
+                    <i class="fa-solid fa-arrow-left-long"></i>
+                </a>                
+            </div>
+            <div class="changeSitePrevious">
+                <img class="ecranPrevious" src="/wp-content/themes/novaneos/assets/images/ecranpc.webp" alt="écran de pc">
+                <div class="imgPrevious">
+                    <img src="<?php echo esc_url($previousMediumUrl); ?>" alt="Image précédente">
+                </div>
+            </div>  
+            <div class="changeSiteNext">  
+                <img class="ecranNext" src="/wp-content/themes/novaneos/assets/images/ecranpc.webp" alt="écran de pc">
+                <div class="imgNext">
+                    <img src="<?php echo esc_url($nextMediumUrl); ?>" alt="Image suivante">
+                </div>                    
+            </div>                
+                        
+            <div class="arrows">
+                <a class="arrow-right" href="<?php echo $nextPostUrl; ?>">
+                    <i class="fa-solid fa-arrow-right-long"></i>
+                </a>                 
+            </div>
+        </div>
 
     </article>
     
--- template_parts/infos_site.php
+++ template_parts/infos_site.php
@@ -5,5 +5,64 @@ $image_url = null;
 
 if (!empty($photo_principale)) {
     $sizes = $photo_principale['sizes'];
-    $image_url = $sizes['full'] ?? $sizes['large'] ?? $sizes['medium'] ?? $sizes['thumbnail'] ?? $photographie['url'];
+    $image_url = $sizes['full'] ?? $sizes['large'] ?? $sizes['medium'] ?? $sizes['thumbnail'] ?? $photo_principale['url'];
+}
+
+// Récupérer le champ personnalisé 'tablette' avec les vérifications
+$tablette = get_field('tablette');
+$img_tablette_url = null;
+
+if (!empty($tablette)) {
+    $sizes = $tablette['sizes'];
+    $img_tablette_url = $sizes['full'] ?? $sizes['large'] ?? $sizes['medium'] ?? $sizes['thumbnail'] ?? $tablette['url'];
+}
+
+// Récupérer le champ personnalisé 'smartphone' avec les vérifications
+$smartphone = get_field('smartphone');
+$img_smartphone_url = null;
+
+if (!empty($smartphone)) {
+    $sizes = $smartphone['sizes'];
+    $img_smartphone_url = $sizes['full'] ?? $sizes['large'] ?? $sizes['medium'] ?? $sizes['thumbnail'] ?? $smartphone['url'];
+}
+
+// Récupérer tous les posts
+$site_posts = get_posts(['post_type' => 'site', 'numberposts' => -1]);
+$current_site_id = get_the_ID();
+
+// Initialiser les URLs par défaut
+$previousPostUrl = $nextPostUrl = '#';
+$previousThumbnailUrl = $nextThumbnailUrl = '';
+
+// Trouver la position du post actuel dans la liste des posts
+$current_key = null;
+foreach ($site_posts as $key => $post) {
+    if ($post->ID === $current_site_id) {
+        $current_key = $key;
+        break;
+    }
+}
+
+// Si un post actuel est trouvé, définir précédent et suivant de manière cyclique
+if ($current_key !== null) {
+    $total_posts = count($site_posts);
+
+    // Calcul cyclique pour les indices précédent et suivant
+    $previous_key = ($current_key - 1 + $total_posts) % $total_posts; // Si au premier, revient au dernier
+    $next_key = ($current_key + 1) % $total_posts; // Si au dernier, revient au premier
+
+    // Récupérer les posts précédent et suivant
+    $previous_post = $site_posts[$previous_key];
+    $next_post = $site_posts[$next_key];
+
+    // Définir les URLs
+    $previousPostUrl = esc_url(get_permalink($previous_post->ID));
+    $nextPostUrl = esc_url(get_permalink($next_post->ID));
+
+    // Définir les miniatures
+    $previous_site = get_field('photo_principale', $previous_post->ID);
+    $previousMediumUrl = !empty($previous_site['sizes']['medium_large']) ? $previous_site['sizes']['medium_large'] : '';
+
+    $next_site = get_field('photo_principale', $next_post->ID);
+    $nextMediumUrl = !empty($next_site['sizes']['medium_large']) ? $next_site['sizes']['medium_large'] : '';
 }
